5fa52d7abc816f5bf36499c92f29643d8f4fc3ba
Kyle Barnhouse <skyline6965@gmail.com>
2019-10-28 19:22:06 -0500

    finished models and public folders

---
+++ public/app.js
@@ -0,0 +1,54 @@
+// get articles, translate to JSON
+$.getJSON("/articles", function (data) {
+    for (var i = 0; i < data.length; i++) {
+        $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    };
+});
+
+// onclick for when user clicks to add note
+$(document).on("click", "p", function () {
+
+    $("#notes").empty();
+
+    var thisId = $(this).attr("data-id");
+
+    $.ajax({
+        method: "GET",
+        url: "/articles/" + thisId
+    })
+        .then(function (data) {
+            console.log(data);
+            $("#notes").append("<h2>" + data.title + "</h2>")
+            $("#notes").append("<input id= 'titleinput' name= 'title' >");
+            $("#notes").append("<textarea id= 'bodyinput' name= 'body' ></textarea>");
+            $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+            // if article already has a note
+            if (data.note) {
+                $("#titleinput").val(data.note.title);
+                $("#bodyinput").val(data.note.body);
+            };
+        })
+});
+
+// onclick save note value
+$(document).on("click", "#savenote", function(){
+
+    var thisId = $(this).attr("data-id");
+
+    $.ajax({
+        method: "POST",
+        url: "/articles/" + thisId,
+        data: {
+            title: $("#titleinput").val(),
+            body: $("#bodyinput").val()
+        }
+    }).then(function(data){
+        console.log(data);
+        $("notes").empty();
+    });
+
+    $("titleinput").val("");
+    $("bodyinput").val("");
+
+});
