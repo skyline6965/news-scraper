05f61f3786ff83deb03dcb155444cfbe02aa1b5f
Kyle Barnhouse <skyline6965@gmail.com>
2019-10-28 21:02:05 -0500

    finished routes

--- server.js
+++ server.js
@@ -20,24 +20,75 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/news-scraper", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost/news-scraper", { useNewUrlParser: true });
 
 // routes
 
 // GET route for scraping website
+app.get("/scrape", function (req, res) {
+    axios.get("/").then(function (response) {
+        var $ = cheerio.load(response.data);
 
+        $("artivle h2").each(function (i, element) {
+            var result = {};
+
+            result.title = $(this)
+                .children("a")
+                .attr("href");
+
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    // console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+        });
+        res.send("Scrape Complete");
+    });
+});
 
 // Get route for getting all articles from db
-
+app.get("/articles", function (req, res) {
+    db.Article.find({})
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
 
 // Get route for grabbing certain articles by id and loading its comment
-
+app.get("/articles/:id", function (req, res) {
+    db.Article.findOne({ _id: req.params.id })
+        .populate("note")
+        .then(function (dbArticle) {
+            res.json(dbArticle)
+        })
+        .catch(function (err) {
+            res.json(err)
+        });
+});
 
 // Post route for saving/updating article's comments
+app.post("/articles/:id", function (req, res) {
+    db.Note.create(req.body)
+        .then(function (dbNote) {
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+        })
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
+
 
 
 
 // start server
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT)
 });
